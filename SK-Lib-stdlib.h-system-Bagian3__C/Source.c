39d0e743bfb542466b4ded4f70824a76826cc071
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-08-30 21:01:06 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-system-Bagian3__C/Source.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+    Source by CPPReference (https://en.cppreference.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main(void) {
+    system("date +%A");
+    system("gcc --version");
+
+    _getch();
+    return 0;
+}
